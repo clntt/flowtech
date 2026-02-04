18ee27dcee6e6a4173fa8474734315d7830a0e10
clnt <c.connery14@gmail.com>
2026-02-04 13:36:10 +0100

    display tag specific questions

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,55 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import { EMPTY_QUESTION } from "@/constants/states";
+import { getTagQuestions } from "@/lib/actions/tag.actions";
+import React from "react";
+
+const TagDetails = async ({ params, searchParams }: RouteParams) => {
+  const { id } = await params;
+  const { page, pageSize, query } = await searchParams;
+
+  const { success, data, error } = await getTagQuestions({
+    tagId: id,
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query,
+  });
+
+  const { tag, questions } = data || {};
+
+  return (
+    <>
+      <section className="flex w-full  flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold ">{tag?.name?.toUpperCase()}</h1>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.TAG(id)}
+          placeholder="Search questions"
+          imgSrc="/icons/search.svg"
+          otherClasses="flex-1"
+        />
+      </section>
+      <DataRenderer
+        success={success}
+        data={questions}
+        error={error}
+        empty={EMPTY_QUESTION}
+        render={(questions) =>
+          questions.map((question) => (
+            <div
+              key={question._id}
+              className="mt-10 flex w-full flex-col gap-6"
+            >
+              <QuestionCard key={question._id} question={question} />
+            </div>
+          ))
+        }
+      />
+    </>
+  );
+};
+
+export default TagDetails;
