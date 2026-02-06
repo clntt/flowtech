15b032acf6e0542282f1eee8c883480481fe4518
clnt <c.connery14@gmail.com>
2026-02-06 07:15:07 +0100

    implement Answer Card UI

---
+++ components/answers/AllAnswers.tsx
@@ -0,0 +1,35 @@
+import { EMPTY_ANSWERS } from "@/constants/states";
+import DataRenderer from "../DataRenderer";
+import AnswerCard from "../cards/AnswerCard";
+
+interface Props extends ActionResponse<Answer[]> {
+  totalAnswers: number;
+}
+
+const AllAnswers = ({ data, success, error, totalAnswers }: Props) => {
+  return (
+    <div className="mt-11">
+      <div className="flex items-center justify-between">
+        {/* <h3>{totalAnswers > 1 ? "Answers" : "Answer"}</h3> */}
+        <h3 className="text-amber-500">
+          {totalAnswers > 1
+            ? `${totalAnswers} Answers`
+            : `${totalAnswers} Answer`}
+        </h3>
+        <p>Filters</p>
+      </div>
+
+      <DataRenderer
+        data={data}
+        success={success}
+        error={error}
+        empty={EMPTY_ANSWERS}
+        render={(answers) =>
+          answers.map((answer) => <AnswerCard key={answer._id} {...answer} />)
+        }
+      />
+    </div>
+  );
+};
+
+export default AllAnswers;
